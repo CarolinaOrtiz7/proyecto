00cc38ed43c60081f7729b82f5dea427187e3853
CarolinaOrtiz <lin4ortiz@gmail.com>
2022-01-13 18:13:06 -0300

    agrego navbar

---
+++ temp1/src/components/CartWidget/CartWidget.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+export const CartWidget = () => {
+    return (
+        <div>
+            <img src="https://www.freepng.es/png-u6b5ot/" alt="" />
+        </div>
+    )
+}
+
+export default CartWidget
---
+++ temp1/src/components/NavBar.jsx
@@ -0,0 +1,41 @@
+import  Navbar  from "react-bootstrap/NavBar"
+import  Container from "react-bootstrap/Container"
+import  Nav  from "react-bootstrap/Nav"
+import  NavDropdown  from "react-bootstrap/NavDropdown"
+import { CartWidget } from "./CartWidget/CartWidget"
+
+
+
+
+const NavBar = () => {
+
+    return(<Navbar bg="dark" variant="dark" expand="lg">
+    <Container>
+      <Navbar.Brand href="#home"> Bajoneros Fast Food</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="me-auto">
+          <Nav.Link href="#home">Home</Nav.Link>
+          <Nav.Link href="#link">Contacto</Nav.Link>
+          <NavDropdown title="Menu" id="basic-nav-dropdown">
+            <NavDropdown.Item href="#action/3.1"></NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.2">Hamburguesas</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.3">Veggie</NavDropdown.Item>
+            <NavDropdown.Divider />
+            <NavDropdown.Item href="#action/3.4">Combos</NavDropdown.Item>
+          </NavDropdown>
+        </Nav>
+      </Navbar.Collapse>
+      
+      <Nav.Link  >
+        <CartWidget/>
+      </Nav.Link>
+    </Container>
+  </Navbar>
+    
+    )
+
+    
+}
+
+export default NavBar
